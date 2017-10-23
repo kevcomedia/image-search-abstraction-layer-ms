5a6199594bb8e864cedd51d74d262caf7eab4d3e
Kev Comedia <kevcomedia@gmail.com>
2017-10-23 16:57:52 +0800

    Write image search route

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+const request = require('request-promise-native');
+
 app.get('/', function(req, res) {
   res.send('Hello world');
+});
+
+app.get('/search', function(req, res) {
+  const cx = process.env.SEARCH_CX;
+  const key = process.env.SEARCH_KEY;
+  const q = req.query.q;
+
+  const options = {
+    uri: 'https://www.googleapis.com/customsearch/v1',
+    qs: {
+      cx,
+      key,
+      q,
+      searchType: 'image',
+    },
+    json: true,
+  };
+
+  request(options)
+      .then(function(data) {
+        res.json(data.items.map((item) => ({
+          url: item.link,
+          alt: item.snippet,
+          context: item.image.contextLink,
+        })));
+      })
+      .catch(function(err) {
+        res.sendStatus(400);
+      });
 });
 
 module.exports = app;
